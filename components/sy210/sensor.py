5e9f5a190427608ae66139bc737e780753e479d1
matt19891231 <43802939+matt19891231@users.noreply.github.com>
2025-09-06 22:27:48 +0700

    Update sensor.py

--- components/sy210/sensor.py
+++ components/sy210/sensor.py
@@ -1,7 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
-from esphome.const import UNIT_MICROGRAMS_PER_CUBIC_METER, ICON_CHEMICAL_WEAPON
+from esphome.const import (
+    CONF_ID,
+    UNIT_MICROGRAMS_PER_CUBIC_METER,
+    ICON_CHEMICAL_WEAPON,
+    DEVICE_CLASS_PM25,
+    STATE_CLASS_MEASUREMENT,
+)
 
 DEPENDENCIES = ["uart"]
 
@@ -9,22 +15,52 @@ DEPENDENCIES = ["uart"]
 sy210_ns = cg.esphome_ns.namespace("sy210")
 SY210Sensor = sy210_ns.class_("SY210Sensor", cg.PollingComponent, uart.UARTDevice)
 
-CONFIG_SCHEMA = sensor.sensor_schema(
-    unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
-    icon=ICON_CHEMICAL_WEAPON,
-    accuracy_decimals=0,
-).extend(
-    {
-        cv.GenerateID(): cv.declare_id(SY210Sensor),
-        cv.GenerateID("uart_id"): cv.use_id(uart.UARTComponent),
-    }
-)
+# 配置常量
+CONF_PM25 = "pm25"
+CONF_PM10 = "pm10"
+CONF_PM1_0 = "pm1_0"
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(SY210Sensor),
+    cv.Optional(CONF_PM25): sensor.sensor_schema(
+        unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+        icon=ICON_CHEMICAL_WEAPON,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_PM25,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(CONF_PM10): sensor.sensor_schema(
+        unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+        icon=ICON_CHEMICAL_WEAPON,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_PM25,  # ESPHome uses PM25 class for all PM sensors
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+    cv.Optional(CONF_PM1_0): sensor.sensor_schema(
+        unit_of_measurement=UNIT_MICROGRAMS_PER_CUBIC_METER,
+        icon=ICON_CHEMICAL_WEAPON,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_PM25,
+        state_class=STATE_CLASS_MEASUREMENT,
+    ),
+}).extend(cv.polling_component_schema("60s")).extend(uart.UART_DEVICE_SCHEMA)
+
 
 async def to_code(config):
     # 创建 C++ 对象
-    uart_var = await cg.get_variable(config["uart_id"])
-    var = cg.new_Pvariable(config[cv.GenerateID()], uart_var)
-
-    # 注册组件
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var.pm25_sensor, config)
+    await uart.register_uart_device(var, config)
+
+    # 注册传感器
+    if CONF_PM25 in config:
+        pm25_sensor = await sensor.new_sensor(config[CONF_PM25])
+        cg.add(var.set_pm25_sensor(pm25_sensor))
+
+    if CONF_PM10 in config:
+        pm10_sensor = await sensor.new_sensor(config[CONF_PM10])
+        cg.add(var.set_pm10_sensor(pm10_sensor))
+
+    if CONF_PM1_0 in config:
+        pm1_0_sensor = await sensor.new_sensor(config[CONF_PM1_0])
+        cg.add(var.set_pm1_0_sensor(pm1_0_sensor))
